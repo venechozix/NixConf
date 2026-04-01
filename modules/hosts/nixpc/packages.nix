6cb84f28791e12a3a9f66da804226bf51d3bea75
chozix <venechozix@gmail.com>
2026-04-01 12:17:12 -0300

    remove unused packages and set bases for refactoring

--- modules/hosts/nixpc/packages.nix
+++ modules/hosts/nixpc/packages.nix
@@ -5,16 +5,12 @@
     { config, pkgs, ... }:
     {
 
-      nixpkgs.config.permittedInsecurePackages = [
-        "python3.13-youtube-dl-2021.12.17"
-      ];
       environment.systemPackages = with pkgs; [
         #dev
         android-tools
         libgccjit
         vim
         wget
-        alacritty
         xclip
         bat
         tealdeer
@@ -25,40 +21,35 @@
         python313
         python314
         pyenv
-        python3Packages.sounddevice
         vscodium
         #utils
         file
-        win2xcur
         ncdu
-        htop
-        kdePackages.kdenlive
-        readest
         icu77
+        ntfs3g
+        gsettings-desktop-schemas
+
+        htop
         pavucontrol
         fastfetch
         spotify
         vesktop
         keepassxc
+
         zip
         unzip
         rar
         unrar
         p7zip
+
         kdePackages.dolphin
         wine
         winetricks
         wine64Packages.waylandFull
         obs-studio
-        firefox
         gimp
-        castnow
         vlc
         openrgb-with-all-plugins
-        gsettings-desktop-schemas
-        geekbench
-        scrcpy
-        ntfs3g
         parsec-bin
       ];
 
