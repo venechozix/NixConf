3fb68fb3915dd1d11b6108afed1a761c65ed8ff8
chozix <venechozix@gmail.com>
2026-04-08 23:24:50 -0300

    change libvirtd for virtualbox (why you ask? because i hate myself

--- modules/features/virt.nix
+++ modules/features/virt.nix
@@ -4,14 +4,7 @@
   flake.nixosModules.virt =
     { config, pkgs, ... }:
     {
-
-      programs.virt-manager.enable = true;
-
-      users.groups.libvirtd.members = [ "chozix" ];
-
-      virtualisation.libvirtd.enable = true;
-
-      virtualisation.spiceUSBRedirection.enable = true;
+      virtualisation.virtualbox.host.enable = true;
 
       virtualisation.docker = {
         enable = true;
