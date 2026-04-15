8e312efe4304f837e680a4926a89aa897e1b654a
chozix <venechozix@gmail.com>
2026-04-15 11:43:12 -0300

    mkchromecast firewall rules

--- modules/features/misc.nix
+++ modules/features/misc.nix
@@ -12,6 +12,28 @@
 
       services.avahi.enable = true;
 
+      networking.firewall = {
+        enable = true;
+        allowedTCPPorts = [
+          8008
+          8009
+          8010
+          8443
+        ];
+        allowedUDPPorts = [
+          5353
+          32768
+          60999
+        ];
+        # Or more broadly allow the range mkchromecast uses:
+        allowedTCPPortRanges = [
+          {
+            from = 5000;
+            to = 5100;
+          } # mkchromecast default streaming port
+        ];
+      };
+
       hardware.bluetooth = {
         enable = true;
         powerOnBoot = true;
