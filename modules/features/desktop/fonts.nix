65dd858f3e8f5f1f236d4788a08c0d35781a1c7f
chozix <venechozix@gmail.com>
2026-04-28 22:43:36 -0300

    noto fonts

--- modules/features/desktop/fonts.nix
+++ modules/features/desktop/fonts.nix
@@ -5,6 +5,8 @@
     { pkgs, ... }:
     {
       fonts.packages = with pkgs; [
+        noto-fonts
+        noto-fonts-cjk-sans
         # ===== Monospace / Coding =====
         jetbrains-mono
         fira-code
