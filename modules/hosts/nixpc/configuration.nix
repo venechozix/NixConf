35861968732fb29e2c89bf304931c5424af54dbf
chozix <venechozix@gmail.com>
2026-05-08 17:50:36 -0300

    move cachix

--- modules/hosts/nixpc/configuration.nix
+++ modules/hosts/nixpc/configuration.nix
@@ -15,6 +15,10 @@
         self.nixosModules.syncthing
       ];
 
+      nix.settings = {
+        substituters = [ "https://nix-gaming.cachix.org" ];
+        trusted-public-keys = [ "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4=" ];
+      };
       boot.kernelPackages = pkgs.pkgs.linuxPackages_6_12;
 
       hardware.graphics.enable = true;
