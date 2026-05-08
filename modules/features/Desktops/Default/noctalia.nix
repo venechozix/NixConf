4957f75280f1f19ce7898f3d5a3ea3456af5aebd
chozix <venechozix@gmail.com>
2026-05-08 17:04:41 -0300

    rename myNoctalia to defNoctalia

--- modules/features/Desktops/Default/noctalia.nix
+++ modules/features/Desktops/Default/noctalia.nix
@@ -5,7 +5,7 @@
     { pkgs, ... }:
     {
 
-      packages.myNoctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
+      packages.defNoctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
         inherit pkgs; # THIS PART IS VERY IMPORTAINT, I FORGOT IT IN THE VIDEO!!!
         settings = (builtins.fromJSON (builtins.readFile ./noctalia.json)).settings;
       };
