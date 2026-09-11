db92f7ca301f254b0055ebd21700dc5715f3987a
chozix <venechozix@gmail.com>
2026-09-10 23:10:55 -0300

    modularize default niri configuration

---
+++ modules/features/Desktops/Default/niri/_layout.nix
@@ -0,0 +1,26 @@
+{
+  gaps = 10;
+
+  border = {
+    off = _: { };
+  };
+
+  focus-ring = {
+    width = 2;
+    active-color = "#d3e2e8";
+    inactive-color = "#505050";
+  };
+
+  preset-column-widths = [
+    { proportion = 0.33333; }
+    { proportion = 0.5; }
+    { proportion = 0.66666; }
+  ];
+
+  default-column-width = {
+    proportion = 1;
+  };
+
+  background-color = "transparent";
+
+}
---
+++ modules/features/Desktops/Default/niri/_outputs.nix
@@ -0,0 +1,26 @@
+{
+  "DP-1" = {
+    mode = "1280x1024@75.025";
+    position = _: {
+      props = {
+        x = 1920;
+        y = 0;
+      };
+    };
+    scale = 1;
+    transform = "90";
+  };
+
+  "HDMI-A-1" = {
+    mode = "1920x1080@60.000";
+    position = _: {
+      props = {
+        x = 0;
+        y = 0;
+      };
+    };
+    scale = 1;
+    transform = "normal";
+  };
+
+}
